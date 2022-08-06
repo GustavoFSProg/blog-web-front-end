73c615b3e1536399f3d1b57070f62bc96fad58f2
Gustavo <gustavo.prog40@gmail.com>
2022-08-06 10:55:49 -0300

    register done

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,57 +1,102 @@
 import Header from './components/header/Header'
-import Sidebarleft from './components/sidebar-left/sidebar-left'
-import SidebarRight from './components/sidebar-right/sidebarRight'
 import moment from 'moment'
-// import foto1 from './assets/foto1.png'
-import face from './assets/face-icon.png'
 import api from './api'
 import {
-  ContainerApp,
-  ContainerParagraph,
-  ContainerWrapper,
-  Span,
-  ImgBody,
-  ImgFace,
-  H1,
-  AutorContainer,
-} from './style'
+  Container,
+  ProductContainer,
+  DivListagemProdutos,
+  Input,
+  FormContainer,
+  Label,
+  InputFile,
+  Button,
+} from './styled-register'
 import { useEffect, useState } from 'react'
 
 function Register() {
   const [post, setPosts] = useState([])
 
-  function getDateWithoutTime(date) {
-    return moment(date).format('DD-MM-YYYY')
+  const [title, setTitle] = useState('')
+  const [autor, setAutor] = useState('')
+  const [text, setText] = useState('')
+  const [image, setImage] = useState([])
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    try {
+      const data = new FormData()
+
+      data.append('title', title)
+      data.append('text', text)
+      data.append('autor', autor)
+      data.append('image', image)
+
+      await api.post(`/register`, data)
+
+      return alert('Cadastro realizado com sucesso!')
+    } catch (error) {
+      console.log(error)
+      return alert(`Deu erro no front ${error}`)
+    }
   }
 
-  async function handlePosts() {
-    const { data } = await api.get(`/all`)
-
-    setPosts(data)
-  }
-
-  useEffect(() => {
-    handlePosts()
-  }, [])
   return (
     <>
       <Header />
+      <Container>
+        <DivListagemProdutos>
+          <ProductContainer>
+            <div
+              style={{
+                color: '#0059b3',
+                fontFamily: 'Roboto',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <h1>Cadastrar Post</h1>
+            </div>
+            <br />
+            <form onSubmit={handleSubmit}>
+              <FormContainer>
+                <InputFile
+                  type="file"
+                  id="image"
+                  className="botao-imagem"
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+                <Label>Titulo do Post: </Label>
+                <Input id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
 
-      <div
-        style={{
-          width: '98.8vw',
-          height: '100vh',
-          background: '#f2f2f2',
-          display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <h2>
-          Cadastro
-        </h2>
-      </div>
+                <Label>Texto: </Label>
+                <textarea
+                  rows="22"
+                  cols="52"
+                  id="text"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+                <br />
+
+                <Label>Autor: </Label>
+                <Input id="autor" value={autor} onChange={(e) => setAutor(e.target.value)} />
+                <br />
+                {/* {token ? ( */}
+                {/* <Button  className="confirm-Button" type="submit">
+                  Cadastrar
+                </Button> */}
+                {/* ) : (
+                  <h2>Unautorized!!!</h2>
+                )} */}
+                <br />
+                <br />
+              </FormContainer>
+            </form>
+          </ProductContainer>
+        </DivListagemProdutos>
+      </Container>
     </>
   )
 }
---
+++ src/styled-register.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 888px;
+  background: #f2f2f2;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+
+  @media screen and (max-width: 700px) {
+    height: auto;
+  }
+`
+
+export const DivListagemProdutos = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 23px;
+  margin-top: 50px;
+
+  @media screen and (max-width: 700px) {
+    flex-direction: column;
+    margin-top: 10px;
+    margin-left: -32px;
+  }
+`
+export const ProductContainer = styled.div`
+  background: #cce6ff;
+  margin-left: 20px;
+  border-radius: 15px;
+  width: 500px;
+
+  @media screen and (max-width: 700px) {
+    margin-top: 20px;
+  }
+`
+
+export const Input = styled.input`
+  /* background: red; */
+  border-radius: 8px;
+  height: 45px;
+  width: 90%;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  border: 1px solid #0069cc;
+`
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+`
+export const Label = styled.label`
+  margin-left: -350px;
+  font-family: 'Roboto';
+  font-size: 15px;
+  color: #0069cc;
+  margin-top: 10px;
+`
+
+export const InputFile = styled.input`
+  /* background: red; */
+  border-radius: 12px;
+  height: 30px;
+  width: 90%;
+  margin-bottom: 10px;
+  margin-top: 20px;
+`
+export const Button = styled.button`
+  width: 90%;
+  height: 40px;
+  cursor: pointer;
+  background: #b3daff;
+  border: 1px solid #80c1ff;
+  border-radius: 10px;
+  color: #004f99;
+  margin-bottom: 40px;
+  /* display: none; */
+`
