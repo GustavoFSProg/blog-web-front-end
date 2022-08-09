c31d4bc41dd79828357e230ba8bcf8e95dc938dd
Gustavo <gustavo.prog40@gmail.com>
2022-08-09 16:56:47 -0300

    asdfasdf

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -99,17 +99,17 @@ function Header() {
         <LinksContainerMobile>
           <div style={{ marginLeft: '20px', marginTop: '8px' }}>
             <Link style={{
-              textDecoration: 'none', fontSize: '18px', fontFamily: 'Roboto', color: 'white',
+              textDecoration: 'none', fontSize: '20px', fontFamily: 'Roboto', color: 'white',
               cursor: 'pointer'
             }} to="/">
               HOME
             </Link>
           </div>
 
-          <div style={{ marginLeft: '25px', marginBottom: '4px', marginTop: '7px', }}>
+          <div style={{ marginLeft: '25px', marginBottom: '4px', marginTop: '10px', }}>
             <Link
 
-              style={{ cursor: 'pointer', fontSize: '18px', textDecoration: 'none', fontFamily: 'Roboto', color: 'white  ' }}
+              style={{ cursor: 'pointer', fontSize: '20px', textDecoration: 'none', fontFamily: 'Roboto', color: 'white  ' }}
               to="/register"
             >
               CADASTRO
