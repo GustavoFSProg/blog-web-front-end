43342d7127d4f3608d81703213dee99891c81a15
Gustavo <gustavo.prog40@gmail.com>
2022-09-15 09:59:15 -0300

    margin top

--- src/style-profile.jsx
+++ src/style-profile.jsx
@@ -67,7 +67,7 @@ export const ContainerWrapper = styled.div`
   margin-top: 1rem;
 
   @media screen and (max-width: 700px) {
-       margin-top: 5rem;
+       margin-top: 7rem;
 
     width: 100%;
   }
